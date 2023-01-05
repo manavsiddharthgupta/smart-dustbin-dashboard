dd6f752bbe606f0786ea347506adfe7133390721
Manav Gupta <2006468@kiit.ac.in>
2023-01-06 00:05:16 +0530

    added icons and fetching latest data

--- src/data/data.js
+++ src/data/data.js
@@ -16,6 +16,12 @@ export const FetchFun = () => {
   }, []);
 };
 export const data = {
+  latest: {
+    v0: 0,
+    v1: 0,
+    guestSeries_1: [0, 0],
+    guestSeries_2: [0, 0, 0, 0],
+  },
   last_Hour: {
     v0: 25,
     v1: 443,
